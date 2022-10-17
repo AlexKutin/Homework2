6e21a4b0ab219ee119b93c72e795db3109b2be64
Alexey Kutin <kutinalexeydm@gmail.com>
2022-10-17 23:35:58 +0300

    Homework2 - Part2 & Part 3

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,52 @@ public class Main {
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
+        System.out.println();
+
+        var friend = 19;
+        System.out.println("Task4");
+        System.out.println("friend = " + friend);
+        friend += 2;
+        System.out.println("friend = " + friend);
+        friend /= 7;
+        System.out.println("friend = " + friend);
+        System.out.println();
+
+        var frog = 3.5;
+        System.out.println("Task5");
+        System.out.println("frog = " + frog);
+        frog *= 10;
+        System.out.println("frog = " + frog);
+        frog /= 3.5;
+        System.out.println("frog = " + frog);
+        frog += 4;
+        System.out.println("frog = " + frog);
+        System.out.println();
+
+        System.out.println("Task6");
+        var boxerWight1 = 78.2;
+        var boxerWight2 = 82.7;
+        var totalBoxerWeight = boxerWight1 + boxerWight2;
+        System.out.println("boxerWight1 = " + boxerWight1);
+        System.out.println("boxerWight2 = " + boxerWight2);
+        System.out.println("totalBoxerWeight = " + totalBoxerWeight);
+        var weightDifference = boxerWight2 - boxerWight1;
+        System.out.println("weightDifference = " + weightDifference);
+        System.out.println();
+
+        System.out.println("Task7");
+        var weightDifferenceMod = boxerWight2 % boxerWight1;
+        System.out.println("weightDifferenceMod = " + weightDifferenceMod);
+        System.out.println();
+
+        System.out.println("Task8");
+        var totalWorkTimeCompany = 640;
+        var workTime = 8;
+        var countWorkers = totalWorkTimeCompany / workTime;
+        System.out.println("Всего работников в компании – " + countWorkers + " человек");
+        var newCountWorkers = countWorkers + 94;
+        var newWorkTimeCompany = newCountWorkers * workTime;
+        System.out.println("Если в компании работает " + newCountWorkers + " человек, то всего " + newWorkTimeCompany + " часов работы может быть поделено между сотрудниками");
+
     }
 }
